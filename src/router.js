67c98da10af3145d90cb7f3d757e7bda11e0fca1
heinhtetzan <mmsonestopit@gmail.com>
2024-11-10 15:13:54 +0700

    feat: auth pages login, register , dashboard

--- src/router.js
+++ src/router.js
@@ -1,4 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -8,6 +11,18 @@ const router = createBrowserRouter([
   {
     path: "/about",
     element: <div>About</div>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardPage />,
   },
 ]);
 
